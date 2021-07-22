f41f552b5963f904517cf62c7b8cab23b9412fe7
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-22 17:55:36 +0530

    How to properly work with redux without state mutation, redux toolkit installation

--- counter-react-redux-app/src/store/index.js
+++ counter-react-redux-app/src/store/index.js
@@ -11,6 +11,7 @@ const counterReducer = (state = initialState, action) => {
   }
 
   if (action.type === "increase") {
+    // state.counter++ //Never do this, though this will works but it will cause some errors in application
     return {
       counter: state.counter + action.amount,
       showCounter: state.showCounter,
@@ -38,3 +39,5 @@ const store = createStore(counterReducer);
 
 export default store;
 // goto main index.js file there we have wrapped our store to whole app
+//We should add all other states in action types when we update state otherwise that particular state will become undefined and can cause errors.
+//Never mutate the state / never change original state: https://academind.com/tutorials/reference-vs-primitive-values/
