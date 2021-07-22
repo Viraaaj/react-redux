01bf338c0e1d0725bd0ae2c2ba869e6a5257c26c
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-22 14:51:05 +0530

    Using redux in react components, useSelector()

--- counter-react-redux-app/src/components/Counter.js
+++ counter-react-redux-app/src/components/Counter.js
@@ -1,15 +1,21 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
 
 export default Counter;
+
+//we use useSelector() hook because by using that we can use part of stores in components
+// If we are using class based components we can use connect also
