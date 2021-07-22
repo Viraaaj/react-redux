a53483919098fdf3a1d73d2ea9d57c77219a4937
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2021-07-22 14:57:11 +0530

    Dispatching actions from inside components, useDispatch()

--- counter-react-redux-app/src/components/Counter.js
+++ counter-react-redux-app/src/components/Counter.js
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -10,6 +19,11 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
@@ -19,3 +33,6 @@ export default Counter;
 
 //we use useSelector() hook because by using that we can use part of stores in components
 // If we are using class based components we can use connect also
+
+// While using dispatch we need to create respective functions and inside that function use dispatch with action
+// like above in our store we declared type increment and decrement use same as there
